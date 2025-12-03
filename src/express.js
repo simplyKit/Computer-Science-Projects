a21e8982b2365f7af5e168627b9ec87d682373ef
Kit <114443603+simplyKit@users.noreply.github.com>
2025-12-03 13:30:45 -0700

    v0.0.1

--- src/express.js
+++ src/express.js
@@ -1,2 +1,14 @@
 // ./src/express.js: Initialize and manage Express.js
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const settings = require('../../settings.json');
+const PORT = "8080";
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/:action', async (req, res) => {
+
+});
+
+app.listen(PORT, () => logRoute("Server", `Server running on http://localhost:${PORT}`));
