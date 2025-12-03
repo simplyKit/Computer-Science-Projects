a008228428c7d73de9f8664ec251d7e2a4995bba
Kit <114443603+simplyKit@users.noreply.github.com>
2025-12-03 14:15:45 -0700

    v0.0.3

--- src/express.js
+++ src/express.js
@@ -7,8 +7,20 @@ const PORT = "8080";
 const app = express();
 app.use(express.json());
 
-app.get('/api/:action', async (req, res) => {
+/**
+ * EXAMPLE REQUEST BODY!!
+ * {
+ *  "key":"thisisakey.key1"
+ *  "value(o)":"thisisavalue"
+ * }
+ */
 
+app.any('/data/write', async (req, res) => {
+    // Normally we don't accept writes from GET requests, but this should be able to be interacted with by the browser
+});
+
+app.get('/data/write', async (req, res) => {
+    // 
 });
 
 app.listen(PORT, () => logRoute("Server", `Server running on http://localhost:${PORT}`));
